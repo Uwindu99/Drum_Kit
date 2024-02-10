a585d33dd6a5b9bf5fbe44fd7e3b9dc66b7c7689
uwindu <widucck21328@gmail.com>
2024-02-10 11:05:39 +0530

    complete drumkit using javascript

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+var buttonItem = document.querySelectorAll(".drum").length;
+
+
+for (var i=0; i< buttonItem ; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+     
+   var buttonInner= this.innerHTML;
+
+   makeSound(buttonInner);
+
+   butnOn(buttonInner);
+
+    }) ;
+}
+
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    butnOn(event.key);
+})
+
+  function makeSound(key){
+
+   switch (key) {
+    case "w":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+    
+    case "a":
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+        break;
+
+    case "s":
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+
+    case "d":
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+        break;
+
+    case "j":
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+
+    case "k":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+
+    case "l":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        break;
+   
+    default:
+        break;
+   }
+
+    
+}
+
+function butnOn(key1){
+
+var styleButton = document.querySelector("."+ key1);
+styleButton.classList.toggle("pressed");
+}
